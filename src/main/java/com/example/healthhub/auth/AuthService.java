b68f448a9cdcd573ff446305582b8e998c5d94b0
enzomastra <mastraenzo@gmail.com>
2024-09-29 14:26:13 -0300

    #5 fixes

--- src/main/java/com/example/healthhub/auth/AuthService.java
+++ src/main/java/com/example/healthhub/auth/AuthService.java
@@ -4,24 +4,37 @@ import com.example.healthhub.client.Role;
 import com.example.healthhub.client.Client;
 import com.example.healthhub.client.ClientRepository;
 import com.example.healthhub.jwt.JwtService;
+
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final ClientRepository clientRepository;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        UserDetails user = clientRepository.findByEmail(request.getEmail()).orElseThrow(() -> new RuntimeException("Client not found"));
+        String token=jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
         Client client = Client.builder()
                 .name(request.getName())
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .weight(request.getWeight())
                 .height(request.getHeight())
                 .role(Role.USER)
